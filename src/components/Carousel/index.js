8db06d0735d46b0d11434f96b5758b2268375600
ali.klein <alexandraeklein@yahoo.com>
2018-02-20 15:19:57 -0800

    click functions

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,34 +6,59 @@ import styles from './carousel.module';
 
 // eslint-disable-next-line
 class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.textContent);
+  }
   render() {
+    const settings = {
+      wrapAround: true,
+      width: '100vw',
+      slideWidth: 1.0,
+      slidesToShow: 2
+    };
     return (
       <NukaCarousel
         className={`${styles['carousel']}`}
-        wrapAround="true"
-        width="100vw"
-        slideWidth={1.0}
-        slidesToShow={2}
+        {...settings}
       >
         <div className={`${styles['carousel-slide']}`}>
           <h1>1</h1>
         </div>
-        <div className={`${styles['carousel-slide']}`}>
+        <a
+          role="presentation"
+          onClick={this.handleClick}
+          className={`${styles['carousel-slide']}`}
+        >
           <h1>2</h1>
-          <div />
-        </div>
+        </a>
         <div className={`${styles['carousel-slide']}`}>
           <h1>3</h1>
           <div />
         </div>
-        <div className={`${styles['carousel-slide']}`}>
+        <a
+          role="presentation"
+          onClick={this.handleClick}
+          className={`${styles['carousel-slide']}`}
+        >
           <h1>4</h1>
-          <div />
-        </div>
+        </a>
         <div className={`${styles['carousel-slide']}`}>
           <h1>5</h1>
           <div />
         </div>
+        <a
+          role="presentation"
+          onClick={this.handleClick}
+          className={`${styles['carousel-slide']}`}
+        >
+          <h1>6</h1>
+        </a>
       </NukaCarousel>
     );
   }
