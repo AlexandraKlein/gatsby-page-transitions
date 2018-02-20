fedbec5f84c3bf40a740f8ef118acc942a458322
ali.klein <alexandraeklein@yahoo.com>
2018-02-20 14:19:00 -0800

    slick styles

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,42 +1,37 @@
 import React from 'react';
-//import Slider from 'react-slick';
 import NukaCarousel from 'nuka-carousel';
 
 import '@/styles/base.global';
-
-import styles from "./carousel.module";
+import styles from './carousel.module';
 
 // eslint-disable-next-line
 class Carousel extends React.Component {
   render() {
-    const settings = {
-      autoplay: true,
-      autoplayInterval: 1000,
-      slidesToShow: 2,
-      wrapAround: true,
-      cellAlign: 'center'
-    };
     return (
       <NukaCarousel
-        {...settings}
+        className={`${styles['carousel']}`}
+        wrapAround="true"
+        width="100vw"
+        slideWidth={1.0}
+        slidesToShow={2}
       >
         <div className={`${styles['carousel-slide']}`}>
-          <h1>&quot;OH MY GOD, YOU CAN BOOK A RESERVATION AT THE TACO BELL TEST KITCHEN!&quot;</h1>
+          <h1>1</h1>
         </div>
         <div className={`${styles['carousel-slide']}`}>
-          <h1>&quot;A MAGICAL MYSTERY LAND FOR FOOD LOVERS&quot;</h1>
+          <h1>2</h1>
           <div />
         </div>
         <div className={`${styles['carousel-slide']}`}>
-          <h1>&quot;THIS IS BIG.&quot;</h1>
+          <h1>3</h1>
           <div />
         </div>
         <div className={`${styles['carousel-slide']}`}>
-          <h1>&quot;HOLY CHALUPA!&quot;</h1>
+          <h1>4</h1>
           <div />
         </div>
         <div className={`${styles['carousel-slide']}`}>
-          <h1>&quot;BEYONCE ALBUM-DROP STYLE&quot;</h1>
+          <h1>5</h1>
           <div />
         </div>
       </NukaCarousel>
